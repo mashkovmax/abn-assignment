4cf38e0a9949b44d6fdd12e94868805fdd7f4e8a
Max Mashkov <mashkovmax@gmail.com>
2026-07-15 15:13:41 +0200

    test coordinate deep link parsing in wikipedia

--- wikipedia-ios/WikipediaUnitTests/Code/NSUserActivity+WMFExtensionsTest.m
+++ wikipedia-ios/WikipediaUnitTests/Code/NSUserActivity+WMFExtensionsTest.m
@@ -45,4 +45,37 @@
                           @"https://en.wikipedia.org/w/index.php?search=dog&title=Special:Search&fulltext=1");
 }
 
+- (void)testPlacesURLWithoutCoordinates {
+    NSURL *url = [NSURL URLWithString:@"wikipedia://places"];
+    NSUserActivity *activity = [NSUserActivity wmf_activityForWikipediaScheme:url];
+    XCTAssertEqual(activity.wmf_type, WMFUserActivityTypePlaces);
+    XCTAssertNil(activity.userInfo[@"WMFLatitude"]);
+    XCTAssertNil(activity.userInfo[@"WMFLongitude"]);
+}
+
+- (void)testPlacesURLWithCoordinatesAndTitle {
+    NSURL *url = [NSURL URLWithString:@"wikipedia://places?lat=52.3547498&lon=4.8339215&title=Amsterdam"];
+    NSUserActivity *activity = [NSUserActivity wmf_activityForWikipediaScheme:url];
+    XCTAssertEqual(activity.wmf_type, WMFUserActivityTypePlaces);
+    XCTAssertEqualWithAccuracy([activity.userInfo[@"WMFLatitude"] doubleValue], 52.3547498, 0.0000001);
+    XCTAssertEqualWithAccuracy([activity.userInfo[@"WMFLongitude"] doubleValue], 4.8339215, 0.0000001);
+    XCTAssertEqualObjects(activity.userInfo[@"WMFLocationName"], @"Amsterdam");
+}
+
+- (void)testPlacesURLWithNegativeCoordinatesAndNoTitle {
+    NSURL *url = [NSURL URLWithString:@"wikipedia://places?lat=40.4380638&lon=-3.7495758"];
+    NSUserActivity *activity = [NSUserActivity wmf_activityForWikipediaScheme:url];
+    XCTAssertEqual(activity.wmf_type, WMFUserActivityTypePlaces);
+    XCTAssertEqualWithAccuracy([activity.userInfo[@"WMFLatitude"] doubleValue], 40.4380638, 0.0000001);
+    XCTAssertEqualWithAccuracy([activity.userInfo[@"WMFLongitude"] doubleValue], -3.7495758, 0.0000001);
+    XCTAssertNil(activity.userInfo[@"WMFLocationName"]);
+}
+
+- (void)testPlacesURLWithIncompleteCoordinatesIsIgnored {
+    NSURL *url = [NSURL URLWithString:@"wikipedia://places?lat=52.3547498"];
+    NSUserActivity *activity = [NSUserActivity wmf_activityForWikipediaScheme:url];
+    XCTAssertEqual(activity.wmf_type, WMFUserActivityTypePlaces);
+    XCTAssertNil(activity.userInfo[@"WMFLatitude"]);
+}
+
 @end
